bc12434badbbe87411f8be33eee987ff2e210a75
Carlos Aguilera <18aguilerac@gmail.com>
2023-01-26 23:07:28 -0800

    added

---
+++ Easy/14.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+      std::string result;
+      int i = 0;
+      for (const char &chara : strs[0]) {
+        for (int j = 1; j < strs.size(); j++) {
+          if (strs[j][i] != chara) {
+            return result;
+          }
+        }
+        result += chara;
+        ++i;
+      }
+      return result;
+    }
+};
