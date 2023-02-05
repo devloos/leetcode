4d77fd471815c182daa1bb0cbc6de9de346cf974
Carlos Aguilera <18aguilerac@gmail.com>
2023-02-05 12:27:10 -0800

    304 done

--- Medium/304.cpp
+++ Medium/304.cpp
@@ -4,39 +4,41 @@ class NumMatrix {
 
  public:
   NumMatrix(std::vector<std::vector<int>> &m) : matrix(m) {
-    if (matrix.empty()) {
-      return;
-    }
-
     for (auto &row : matrix) {
-      for (int i = 1; i < matrix.size(); ++i) {
+      for (int i = 1; i < row.size(); ++i) {
         row[i] += row[i - 1];
       }
     }
 
-    for (int col = 0; col < matrix.size(); ++col) {
+    for (int col = 0; col < matrix[0].size(); ++col) {
       for (int row = 1; row < matrix.size(); ++row) {
         matrix[row][col] += matrix[row - 1][col];
       }
     }
-
-    // matrix.insert(matrix.begin(), std::vector<int>(matrix.size(), 0));
-    // for (auto &row : matrix) {
-    //   row.insert(row.begin(), 0);
-    // }
   }
 
-  int sumRegion(int row1, int col1, int row2, int col2) {
-    // if (row1 == row2 && col1 == col2) {
-    //   return matrix[row1][col1];
-    // }
-    return matrix[row2][col2] - matrix[row1][col2] - matrix[row2][col1] +
-           matrix[row1][col1];
+  int sumRegion(int r1, int c1, int r2, int c2) {
+    int total = matrix[r2][c2];
+    int extra = 0;
+
+    if (c1 - 1 >= 0) {
+      extra += matrix[r2][c1 - 1];
+    }
+
+    if (r1 - 1 >= 0) {
+      extra += matrix[r1 - 1][c2];
+    }
+
+    if (r1 - 1 >= 0 && c1 - 1 >= 0) {
+      extra -= matrix[r1 - 1][c1 - 1];
+    }
+
+    return total - extra;
   }
 };
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ * int param_1 = obj->sumRegion(r1,c1,r2,c2);
  */
