3f5f3e9def6ee8d8a6fecf46b883ecb57b08b1ae
Carlos Aguilera <18aguilerac@gmail.com>
2023-02-16 21:00:21 -0800

    11

---
+++ Medium/11.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  int maxArea(vector<int> &height) {
+    int mav = 0;
+    int left = 0;
+    int right = height.size() - 1;
+    for (int i = 0; i < height.size(); ++i) {
+      int dis = right - left;
+      int smaller = (height[left] > height[right]) ? right-- : left++;
+      mav = max(mav, height[smaller] * dis);
+    }
+
+    return mav;
+  }
+};
